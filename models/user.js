1a1547d40782b8bbde0a6d3e550b5ee9b9e9ea8b
alimacattack <alimac70117@gmail.com>
2016-12-02 14:47:46 -0500

    Add zipCode to User model

--- models/user.js
+++ models/user.js
@@ -24,6 +24,9 @@ module.exports = function(sequelize, DataTypes) {
         isEmail: true,
         notEmpty: true
       }
+    },
+    zipCode: {
+      type: DataTypes.STRING
     }
   }, {
     instanceMethods: {
